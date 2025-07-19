05c8d6be1e0c1e5eb96373839d714e31c281da65
陳威凱 Wayne <wayne900719@gmail.com>
2025-07-19 23:16:07 +0800

    Time: 103 ms (40.06%) | Memory: 32.3 MB (41.47%) - LeetSync

--- 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.py
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.py
@@ -1,11 +1,12 @@
 class Solution:
     def removeSubfolders(self, folder: List[str]) -> List[str]:
+        # sort with the length of the string
         new_folder = []
         for item in folder:
             new_folder.append((len(item), item))
         new_folder.sort()
-        # print(new_folder)
 
+        # use set to check if start with the folder
         folder_set = set()
         for length, folders in new_folder:
             idx = 1
@@ -13,7 +14,6 @@ class Solution:
                 if folders[idx] == "/" and folders[:idx] in folder_set:
                     break
                 idx += 1
-
             else:
                 folder_set.add(folders)
         return list(folder_set)
